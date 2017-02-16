e7c32f7365106d7cd0c21dbae82d450ee5761db7
Nicholas Michel <nickmichel100@gmail.com>
2017-02-16 14:32:56 -0500

    progression of redesign, centered front page content, fixed whitespace bugs, added photo, working on colors and other aesthetics.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import BigSquare from './BigSquare.js'
 import SmallSquare from './SmallSquare.js'
 import content from "./content"
-import headshot from './headshot.jpg'
+import headshot from './unnamed.jpg'
 
 class App extends Component {
 
@@ -33,13 +33,12 @@ class App extends Component {
     if(this.state.bigSquare===false){
       return (
         <div className="App">
-
           <div id="square-space">
             <SmallSquare fullscreen={this.fullscreener} id="about" squareNum="0" smallContent={content[0].small} />
             <SmallSquare fullscreen={this.fullscreener} id="skills"squareNum="1" smallContent={content[1].small} />
             <div className="App-header">
+              <h2 id="tagline">Nick Michel: Web Developer</h2>
               <img src={headshot} className="App-logo" alt="logo" />
-              <h2>Nick Michel: Web Developer</h2>
             </div>
             <SmallSquare fullscreen={this.fullscreener} id="projects" squareNum="2" smallContent={content[2].small} />
             <SmallSquare fullscreen={this.fullscreener} id="contact" squareNum="3" smallContent={content[3].small} />

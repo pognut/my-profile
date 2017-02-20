87254ce0755f6d05dcec18f17097772d7bf16d4d
Nicholas Michel <nickmichel100@gmail.com>
2017-02-20 13:51:11 -0500

    fixed border bug

--- src/content.js
+++ src/content.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import tourpic from './tourpic.png'
 import beespic from './beespic.png'
-var tourimage = require('./tourpic.png')
 
 // To do:
 // 1) refine content (revise writing, screenshot projects) (last step before usability)
@@ -10,13 +9,10 @@ var tourimage = require('./tourpic.png')
 // 4) why you should hire me section (memory line should have joke about never losing trivia night)
 // 5) clean up react so no if statement in return, all content should be loaded through big tile(?), clean up spare files and rename obvious create-react-app files
 // 6) DONE resume display/download (in about me)
-
-
-
-// headers all caps
-// center headers
-// remove all small square content except headers
-// try centered picture with header surrounding
+//random fixes
+// make the back button more obvious/accessable
+// move front page stuff closer together(?)
+// fix the blurry tour pic
 
 
 var smallAbout=[
@@ -34,7 +30,7 @@ var smallContact=[
 ]
 
 var bigAbout=[
-  <h1>About me, in detail</h1>,
+  <h2 className="big-header">About me, in detail</h2>,
   <p>Hello, I'm Nick! I graduated from Vassar College with a BA in Science and Technology studies. After a number
   of different sales and customer service jobs, including leading walking tours of New York City, I decided to take
   the plunge and become a web developer. Six months and a stint as a teaching assistant at my old bootcamp later, here I am,
@@ -49,7 +45,7 @@ var bigAbout=[
 
 
 var bigSkills=[
-<h2>More skills, for more bills. So many bills...</h2>,
+<h2 className="big-header">So many skills. So many bills...</h2>,
 <h3>Known Skills</h3>,
 <ul id = "known">
   <li>HTML, CSS, JS (The basics)</li>
@@ -73,15 +69,15 @@ var bigSkills=[
 ]
 
 var bigProjects=[
-<h2>Past Projects</h2>,
+<h2 className="big-header">Past Projects</h2>,
 <div>
-  <img src={tourimage} />
+  <a href="https://google-tours.herokuapp.com" target="_blank"><img src={tourpic} /></a>
   <p>This is Google Tours (name subject to change), my final project at General Assembly, the bootcamp that taught me web development.
   Google Tours was inspired by my time leading walking tours in New York City, and lets users create their own custom tours by applying
   written text blurbs to google street view panoramas. The site is built using a Rails backend, and while the live version front end was
   created with vanilla JavaScript, I'm currently refactoring it to use ReactJS.
   </p>
-  <img src={require('./beespic.png')}/>
+  <a href="http://www.beehavenbees.com" target="_blank"><img src={beespic}/></a>
   <p>This is Bee Haven Bees, a beekeeping service based out of Glen Ridge NJ. A relative of mine runs Bee Haven Bees, and asked me to
   help him with a site redesign. (I know you've had this happen to you before, don't lie) Being the kind soul that I am, I helped him out,
   which provided me a wealth of experience with CSS and basic site design. It's not fancy, but it's my first true website and I'm damn proud of it.
@@ -89,7 +85,7 @@ var bigProjects=[
 </div>]
 
 var bigContact=[
-<h2>Here's how you can get in touch</h2>,
+<h2 className="big-header">Here's how you can get in touch</h2>,
 <div>
 <h4>By email: <a href="mailto:nickmichel100@gmail.com">nickmichel100@gmail.com</a></h4>
 <h4>Or phone: 561-596-0368</h4>
